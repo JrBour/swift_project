bc20aaa4e7dd3b9a485bd3241e7966a074255bc2
Christopher <christopher.loubassou@hotmail.fr>
2018-06-08 01:02:24 +0200

    Home quiz

--- swift_project/Controller/HomeViewController.swift
+++ swift_project/Controller/HomeViewController.swift
@@ -7,7 +7,17 @@ class HomeViewController: UIViewController {
     var currentUser: Firebase.User?
     let firebaseAuth = Auth.auth()
     
+    // Set Outlet and actions
+    @IBOutlet weak var quizImage: UIImageView!
+    @IBOutlet weak var challengeBlock: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        quizImage.image = UIImage(named: "quiz_image")
+        
+        challengeBlock.layer.shadowColor = UIColor.black.cgColor
+        challengeBlock.layer.shadowOpacity = 0.2
+        challengeBlock.layer.cornerRadius = 10
+        challengeBlock.layer.shadowOffset = CGSize.zero
     }
 }
