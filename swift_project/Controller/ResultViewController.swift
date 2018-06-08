8445793fd0723dc82f5ce8fe931a3c43db7b8086
Christopher <christopher.loubassou@hotmail.fr>
2018-06-08 02:45:14 +0200

    ResultController and storyboard

---
+++ swift_project/Controller/ResultViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+class ResultViewController: UIViewController  {
+    
+    // Outlet and actions
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var pictureUser: UIImageView!
+    @IBOutlet weak var experiencePoints: UILabel!
+    @IBOutlet weak var victoryNumber: UILabel!
+    @IBOutlet weak var nulNumber: UILabel!
+    @IBOutlet weak var defeatNumber: UILabel!
+    @IBOutlet weak var nextChallenge: RoundButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // Change resultLabel when victory or not
+    
+    // Win or lose point with exp Points
+    
+    // Display victorynumber, nul and defeats
+    
+    // Next challenge = facePlayer storyboard
+}
